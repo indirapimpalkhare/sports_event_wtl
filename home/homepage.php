9eae6e6bf5889ae1dd18f4fd4f0f79dbe52d6ec4
indirapimpalkhare <indirapimpalkhare@users.noreply.github.com>
2019-12-29 18:54:57 +0530

    homepage done with navbar. logout added

--- home/homepage.php
+++ home/homepage.php
@@ -9,31 +9,58 @@
   <!--we use style tag for internal css inside head tag-->
 
 </head>
-<body>
-
-<div class = "container" style="margin-top:50px">
 <?php
-require_once("../dbconn.php");
-//$sql = "SELECT id, fname, lname FROM events";
-//$result = $conn->query($sql);
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    echo '<div class="alert alert-warning">You need to log in first! Redirecting to log in page.</div>';
+	header("Refresh: 1; URL=/sports_event/login/log_in.php");
+    exit;
+}
+?>
+<body>
+<div class = "container">
+<?php
+echo file_get_contents("../navbar.html");
+//echo "I tried";
 ?>
 
+<div class = "container" style="margin-top:50px">
 
-<div class="card" style="width: 100%;">
-	<div class="card-body">
-		<img class="card-img-top" src="..." alt="Card image cap">  
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">Event Name</li>
-    <li class="list-group-item">Event Dates</li>
-    <li class="list-group-item">Registration Fees</li>
-  </ul>
-  <div class="card-body">
-    <a href="#" class="card-link">Register</a>
-  </div>
+<?php
+require_once("../dbconn.php");
+$sql = "SELECT * FROM events";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+	// output data of each row
+	while($row = $result->fetch_assoc()) {
+		echo "<div class='card' style='width: 100%;'>";
+		echo "<div class='card-body'>";
+		echo "<h5 class='card-title'>" .$row["event_name"] . "</h5>";
+		echo "<p class='card-text'>" . $row["sport_details"] . "</p>";
+		echo "</div>";
+		echo "<ul class='list-group list-group-flush'>";
+		echo "<li class='list-group-item'> <b> Start Date : </b>" . $row["start_date"] . "</li>";
+		echo "<li class='list-group-item'><b> End Date : </b>" . $row["end_date"] . "</li>" ;
+		echo "<li class='list-group-item'><b> Registration Fees : </b>" . $row["reg_fees"] . "</li>" ;
+		echo "</ul>";
+		echo "<div class='card-body'> <a href='http://" . $row["link"] . "' class='card-link' style = 'color: lightcoral;'><b> Register Now! </b> </a>";
+		echo "</div>";
+		echo "</div>";
+		echo "<hr>";
+	}
+} 
+else {
+	echo "0 results";
+}
+$conn ->close();
+?>
+  <!--img class="card-img-top" src="..." alt="Card image cap"-->
 </div>
-
-
+</div>
 </div>
 </body>
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
--- sports_event_sql_exp.sql
+++ sports_event_sql_exp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 28, 2019 at 06:23 AM
+-- Generation Time: Dec 29, 2019 at 02:21 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
 
@@ -21,6 +21,34 @@ SET time_zone = "+00:00";
 --
 -- Database: `sports_event`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `event_id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `event_name` varchar(20) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `reg_fees` int(11) NOT NULL,
+  `reg_last_date` date DEFAULT NULL,
+  `prize_money` int(11) NOT NULL,
+  `link` varchar(50) NOT NULL,
+  `sport_details` text NOT NULL,
+  `address` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`event_id`, `username`, `event_name`, `start_date`, `end_date`, `reg_fees`, `reg_last_date`, `prize_money`, `link`, `sport_details`, `address`) VALUES
+(1, 'Indira1234', 'Summit', '2019-09-14', '2019-09-18', 1500, '2019-09-10', 50000, 'summit.mitwpu.edu.in', 'Summit is a National level inter engineering sports event. Events include basketball, volleyball, cricket, football.', 'MITWPU - Pune 411038'),
+(2, 'Indira1234', 'Summit', '2019-09-14', '2019-09-18', 1500, '2019-09-10', 50000, 'summit.mitwpu.edu.in', 'Summit is a National level inter engineering sports event. Events include basketball, volleyball, cricket, football.', 'MITWPU - Pune 411038');
 
 -- --------------------------------------------------------
 
@@ -48,12 +76,39 @@ INSERT INTO `users` (`username`, `pwd`, `college_name`, `college_city`) VALUES
 --
 
 --
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`event_id`),
+  ADD KEY `username` (`username`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`username`),
   ADD UNIQUE KEY `username` (`username`),
   ADD KEY `username_2` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `events`
+--
+ALTER TABLE `events`
+  ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
